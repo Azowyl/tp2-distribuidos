3a71ca054564923e0581113bfd61d71ee485ee8d
Facundo Gerstner <fgerstner@devartis.com>
2019-05-26 16:44:13 -0300

    add script for 4.3.1

---
+++ src/4.3.1.py
@@ -0,0 +1,25 @@
+import pybgpstream
+import pprint
+
+time_init = "2017-03-01"
+time_end = "2017-03-01 00:01"
+
+collector = 'route-views.saopaulo' # route-views.chicago
+
+target_as = 262907 # 27747 (Telecentro)
+
+stream = pybgpstream.BGPStream(
+    from_time=time_init ,
+    until_time=time_end ,
+    filter = "type ribs and collector %s and path %s" % (
+        collector,
+        target_as
+    )
+)
+
+as_paths = set()
+for elem in stream:
+    if int(elem.fields["as-path"].split(' ')[-2]) == target_as:
+        as_paths.add(elem.fields["as-path"])
+
+pprint.pprint(len(as_paths))

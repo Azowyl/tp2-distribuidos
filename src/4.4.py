5f3f34a57bb0e3aa6043cb74dff11e11a8cf9f65
Facundo Gerstner <fgerstner@devartis.com>
2019-06-02 12:28:02 -0300

    remove target as

--- src/4.4.py
+++ src/4.4.py
@@ -4,16 +4,13 @@ import pprint
 time_init = "2015-04-29"
 time_end = "2015-04-29 00:01"
 
-collector = 'route-views.saopaulo' # route-views.chicago
-
-target_as = 262907 # 27747 (Telecentro)
+collector = 'route-views.kixp' # route-views.chicago
 
 stream = pybgpstream.BGPStream(
     from_time=time_init ,
     until_time=time_end ,
-    filter = "type ribs and collector %s and path %s" % (
+    filter = "type ribs and collector %s" % (
         collector,
-        target_as
     )
 )
 
